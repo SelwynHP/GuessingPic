6d5bf502d22aebcf2233d655cf0ac8b790343e0f
SelwynHP <61285762+SelwynHP@users.noreply.github.com>
2020-03-13 00:01:12 -0400

    Finalized Code - In Working State

--- app/src/main/java/com/artemis/guesspic/GameActivity.java
+++ app/src/main/java/com/artemis/guesspic/GameActivity.java
@@ -2,13 +2,49 @@ package com.artemis.guesspic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+
+    int[] a = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.ee,R.drawable.f};
+    Random ran = new Random();
+    int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final int choice = ran.nextInt(a.length);
+
+        CountDownTimer cdt = new CountDownTimer(15000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                TextView textView = (TextView) findViewById(R.id.textViewCounter);
+                textView.setText(millisUntilFinished / 1000 + "");
+                ImageView imageView = (ImageView) findViewById(R.id.imageViewMain);
+                int i = ran.nextInt(a.length);
+                imageView.setImageResource(a[i]);
+                if(i == choice){
+                    counter++;
+                }
+            }
+
+            public void onFinish() {
+                TextView textView = (TextView) findViewById(R.id.textViewCounter);
+                textView.setText("Done!");
+
+                Intent goToResult = new Intent(getApplicationContext(),ResultActivity.class);
+                goToResult.putExtra("counter",counter);
+                goToResult.putExtra("choice",a[choice]);
+                startActivity(goToResult);
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/artemis/guesspic/ResultActivity.java
+++ app/src/main/java/com/artemis/guesspic/ResultActivity.java
@@ -2,7 +2,16 @@ package com.artemis.guesspic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +19,27 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        final int choice = getIntent().getExtras().getInt("choice");
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(choice);
+
+        final Button buttonResult = (Button) findViewById(R.id.buttonResult);
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextGuess = (EditText) findViewById(R.id.editTextGuess);
+                int userGuess = Integer.parseInt(editTextGuess.getText().toString());
+                int counter = getIntent().getExtras().getInt("counter");
+                TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
+                if(userGuess == counter){
+                    textViewResult.setText("Success");
+                    buttonResult.setEnabled(false);
+                }
+                else{
+                    textViewResult.setText("Wrong!");
+                }
+            }
+        });
     }
 }
